57fd90f6578702f64241f27693d960ddd29b1897
marmat001 <markusmatu@Markuss-MacBook-Pro.local>
2021-02-08 09:44:33 +0100

    added logout functionality and the ability to show username in navbar

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,39 +1,58 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { LinkContainer } from 'react-router-bootstrap'
 
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
+import { logout } from '../actions/userActions'
 
 const Navigation = () => {
+	const dispatch = useDispatch()
+	const userLogin = useSelector((state) => state.userLogin)
+	const { userInfo } = userLogin
+
+	const logoutHandler = () => {
+		dispatch(logout())
+	}
+
 	return (
 		<header>
 			<Navbar className='fixed-top' style={{ backgroundColor: '#00bc8c' }} expand='lg' collapseOnSelect>
 				<Container>
 					<LinkContainer to='/'>
-						<Navbar.Brand>
-							<i className='fas fa-tachometer-alt mx-1' /> <strong style={{ color: 'white' }}>Vroom</strong>
+						<Navbar.Brand id='nav-item'>
+							<i className='fas fa-tachometer-alt mx-1' /> <strong>Vroom</strong>
 						</Navbar.Brand>
 					</LinkContainer>
 					<Navbar.Toggle aria-controls='basic-navbar-nav' />
 					<Navbar.Collapse id='basic-navbar-nav'>
 						<Nav className='ml-auto'>
 							<LinkContainer to='/vehicles'>
-								<Nav.Link className='mx-3'>
+								<Nav.Link id='nav-item' className='mx-3'>
 									<i className='fas fa-car mx-1' />
-									<strong style={{ color: 'white' }}>Vehicles</strong>
+									<strong>Vehicles</strong>
 								</Nav.Link>
 							</LinkContainer>
 							<LinkContainer to='/cart'>
-								<Nav.Link className='mx-3'>
+								<Nav.Link id='nav-item' className='mx-3'>
 									<i className='fas fa-shopping-cart mx-1' />
-									<strong style={{ color: 'white' }}>Cart</strong>
+									<strong>Cart</strong>
 								</Nav.Link>
 							</LinkContainer>
-							<LinkContainer to='/login'>
-								<Nav.Link className='mx-3'>
-									<i className='fas fa-user mx-1' />
-									<strong style={{ color: 'white' }}>Sign In</strong>
-								</Nav.Link>
-							</LinkContainer>
+							{userInfo ? (
+								<NavDropdown title={userInfo.name} id='username'>
+									<LinkContainer to='/profile'>
+										<NavDropdown.Item>Profile</NavDropdown.Item>
+									</LinkContainer>
+									<NavDropdown.Item onClick={logoutHandler}>Log Out</NavDropdown.Item>
+								</NavDropdown>
+							) : (
+								<LinkContainer to='/login'>
+									<Nav.Link id='nav-item' className='mx-3'>
+										<i className='fas fa-user mx-1' />
+										<strong>Sign In</strong>
+									</Nav.Link>
+								</LinkContainer>
+							)}
 						</Nav>
 					</Navbar.Collapse>
 				</Container>
