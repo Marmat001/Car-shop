fccf3b77ce7563fa5787ef40cbc9f6485c731bb6
marmat001 <markusmatu@Markuss-MacBook-Pro.local>
2021-05-08 02:36:55 +0200

    modified server code for deployment and added a few scripts

--- backend/server.js
+++ backend/server.js
@@ -17,25 +17,35 @@ connectDB()
 const app = express()
 
 if (process.env.NODE_ENV === 'development') {
-	app.use(morgan('dev'))
+  app.use(morgan('dev'))
 }
 
 app.use(express.json())
-
-app.get('/', (req, res) => {
-	res.send('API is running...')
-})
 
 app.use('/api/vehicles', carRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
 app.use('/api/upload', uploadRoutes)
 
-app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
+app.get('/api/config/paypal', (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+)
 
 const __dirname = path.resolve()
 
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...')
+  })
+}
 
 app.use(notFound)
 
@@ -43,4 +53,9 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(
+  PORT,
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  )
+)
